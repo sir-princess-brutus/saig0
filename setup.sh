184fece182f7a881586f64e2a38182d84b7a5941
Sir Princess Brutus, Photon <sirprincessbrutus@gmail.com>
2020-12-13 15:56:47 +0000

    Added daemon to setup.

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 echo "cp saig0.php /var/www/html/saig0"
 cp saig0.php /var/www/html/saig0
+
+echo "cp saig0_lib.py random_play_daemon.py /var/www/saig0_daemon/"
+cp saig0_lib.py random_play_daemon.py /var/www/saig0_daemon/
