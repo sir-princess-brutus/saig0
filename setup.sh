8c1136e93a5928d6bc16bf2cfb8fe6229b916fcb
Sir Princess Brutus, Photon <sirprincessbrutus@gmail.com>
2020-12-13 21:02:45 +0000

    Added lib file t phplib.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo "cp saig0.php /var/www/html/saig0"
+echo ""
 cp saig0.php /var/www/html/saig0
+
+echo "cp saig0_gameplay.php /var/www/phplib"
+echo ""
+cp saig0_gameplay.php /var/www/phplib
 
 echo "cp saig0_lib.py random_play_daemon.py /var/www/saig0_daemon/"
 cp saig0_lib.py random_play_daemon.py /var/www/saig0_daemon/
